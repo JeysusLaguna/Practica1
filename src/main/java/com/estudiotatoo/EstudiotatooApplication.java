0290c201a235d9fe54a279b4b4ba446948f54dd5
JeysusLaguna <75816115+JeysusLaguna@users.noreply.github.com>
2022-02-17 22:32:51 -0600

    Practica de semana 4

---
+++ src/main/java/com/estudiotatoo/EstudiotatooApplication.java
@@ -0,0 +1,13 @@
+package com.estudiotatoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiotatooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiotatooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudiotatoo/EstudiotatooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudiotatoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiotatooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
